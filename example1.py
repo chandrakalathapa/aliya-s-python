6a36f2c021f9b3fa7f5befc8b4155928a442626b
chandrakalathapa <106816332+chandrakalathapa@users.noreply.github.com>
2022-06-14 18:38:39 +0530

    Initial commit

---
+++ example1.py
@@ -0,0 +1,8 @@
+from turtle import *
+s = getscreen()
+t = Turtle()
+t.speed('fastest')
+for i in range (11):
+    t.fd(100)
+    t.lt(72)
+mainloop()    
---
+++ example5.py
@@ -0,0 +1,11 @@
+from turtle import*
+s=getscreen()
+t=Turtle()
+t.speed('fastest')
+for i in range(10):
+    t.fd(100)
+    t.it(60)
+    t.circle(50,180)
+    t.dot(20)
+
+mainloop()    
